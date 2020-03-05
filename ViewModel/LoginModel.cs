5c58c1a2cf6035ae793c8e84f0ab5d840daf5613
Manish Kumar Burnwal <manish.burnwal28@gmail.com>
2020-03-05 18:57:44 +0530

    Add files via upload

---
+++ ViewModel/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2FAGoogleAuthenticator.ViewModel
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class MFAModel
+    {
+        public string BarcodeImageUrl { get; set; }
+
+        public string SetupCode { get; set; }
+
+        public bool UniqueKey { get; set; }
+
+        public string Passcode { get; set; }
+
+        public bool status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
